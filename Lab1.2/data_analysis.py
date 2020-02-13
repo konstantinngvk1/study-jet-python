662a42414e3cbf31a4a0f4d3ade9c5b0092354b1
aarask <andrey.rask@gmail.com>
2020-02-13 17:26:45 +0300

    New Lab

--- Lab1.2/data_analysis.py
+++ Lab1.2/data_analysis.py
@@ -1,8 +1,18 @@
-# Libraries import
 from matplotlib import pyplot
 from openpyxl import load_workbook
+
 wb = load_workbook('data_analysis_lab.xlsx')
 sheet = wb['Data']
-sheet['A'][1:]
+
+
 def getvalue(x): return x.value
-map(getvalue, sheet['A'][1:])
+
+
+list_x = list(map(getvalue, sheet['A'][1:]))
+list_y1 = list(map(getvalue, sheet['C'][1:]))
+list_y2 = list(map(getvalue, sheet['D'][1:]))
+
+pyplot.plot(list_x, list_y1, label='Уровень')
+pyplot.plot(list_x, list_y2, label='Активность Солнца')
+
+pyplot.show()
---
+++ Lab1.3/snmp_get.py
@@ -0,0 +1,29 @@
+from pysnmp.hlapi import *
+
+snmp_object_1 = ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)
+snmp_object_2 = ObjectIdentity('1.3.6.1.2.1.2.2.1.2')
+
+ip = '10.31.70.107'
+port = '161'
+
+result1 = getCmd(SnmpEngine(),
+  CommunityData('public', mpModel=0),
+  UdpTransportTarget((ip, port)),
+  ContextData(),
+  ObjectType(snmp_object_1))
+
+result2 = nextCmd(SnmpEngine(),
+    CommunityData('public', mpModel=0),
+    UdpTransportTarget((ip, port)),
+    ContextData(),
+    ObjectType(snmp_object_2),
+    lexicographicMode=False)
+
+
+print("\nЗапрос данных с '%s'..." % ip)
+for y in result1:
+    for str in list(y)[3]: print(str)
+
+print("\n\nЗапрос MIB")
+for y in result2:
+    for str in list(y)[3]: print(str)
