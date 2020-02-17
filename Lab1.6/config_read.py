1a19f7f2e1ade06913cb16f4b95b214924bed28b
aarask <andrey.rask@gmail.com>
2020-02-17 13:34:03 +0300

    Initial Test

--- Lab1.6/config_read.py
+++ Lab1.6/config_read.py
@@ -2,3 +2,17 @@ import ipaddress, re
 from glob import glob as glob
 
 
+def Classifier(configs):
+    ips = []
+    interfaces = []
+    hosts = []
+    for f in configs:
+        with open(f) as file:
+            for s in file:
+                if re.match("^( ip address) ([0-9]{1,3}\.?){4} ([0-9]{1,3}\.?){4}$", s):
+                    s = s.replace("ip address ", "")
+                    t = re.split(" ", s)
+                    net = ipaddress.IPv4Network((t[1], t[2].rstrip()), 0)
+                    ip = {"ip": net}
+
+
