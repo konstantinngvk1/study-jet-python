98ad70cb6c386be23cd0d1e8158540971f220e37
aarask <andrey.rask@gmail.com>
2020-02-19 11:46:54 +0300

    Initial Test

--- Lab2.2/webserv.py
+++ Lab2.2/webserv.py
@@ -2,39 +2,39 @@ import re
 import glob
 from flask import Flask, jsonify
 
-filename = "config_files\*.txt" # где лежат конфиги
+#filename = "config_files\*.txt"
 
-def getdatafromconfigs(filename): # создаем функцию
+def getdatafromconfigs(filename):
     result = {}
-    for filename in glob.glob(filename): # цикл на чтение из файла
+    for filename in glob.glob(filename):
         ipinfo = []
-        for line in open(filename): # цикл для построчного считывания из файла
-            pat = "(ip address) ((?:[0-9]{1,3}[\.]){1,3}[0-9]{1,3}) ((?:[0-9]{1,3}[\.]){3}[0-9]{1,3})" # регулярное выражение для определения IP адресов
+        for line in open(filename):
+            pat = "(ip address) ((?:[0-9]{1,3}[\.]){1,3}[0-9]{1,3}) ((?:[0-9]{1,3}[\.]){3}[0-9]{1,3})"
             m = re.match(pat, line.strip().lower())
             if m:
-                ipaddr = m.group(2) + "/" + m.group(3) # айпи и маска выводится через слеш
-            if re.match("(hostname)", line.strip().lower()):
+                ipaddr = m.group(2) + "/" + m.group(3)
                 ipinfo.append(ipaddr)
+            if re.match("(hostname)", line.strip().lower()):
                 hname = line.strip().lower().split(" ")[1]
         result[hname] = list(set(ipinfo))
     return result
 
-res = getdatafromconfigs(filename)
+res = getdatafromconfigs("config_files\*.txt")
 
 
-app = Flask(__name__) # запуск вебсервера
+app = Flask(__name__)
 
-@app.route("/") # главная
+@app.route("/")
 @app.route("/index")
 def index():
-    return 'Go to <a href="/configs">/configs</a> to see the hostnames; <br><br>' \ # Красиво с HTML
+    return 'Go to <a href="/configs">/configs</a> to see the hostnames; <br><br>' \
            'Go to /config/[name] to see [name] config. Thanks!'
 
-@app.route("/configs") # список хостов с конфигами
+@app.route("/configs")
 def configs():
     return jsonify(list(res.keys()))
 
-@app.route("/config/<name>") # отобразить IP / MASK по хостнейму
+@app.route("/config/<name>")
 def config(name):
     try:
         return jsonify(res[name])
