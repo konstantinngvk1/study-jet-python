86579af48e9d15af8e5b73092a3f111652462759
aarask <andrey.rask@gmail.com>
2020-02-19 10:50:40 +0300

    Initial Test

--- Lab2.2/webserv.py
+++ Lab2.2/webserv.py
@@ -1,48 +1,45 @@
-from flask import Flask, jsonify
 import re
 import glob
+from flask import Flask, jsonify
 
-def Classifier(configs):
 
-    hosts = {}
+def getdatafromconfigs(fname):
+    result = {}
+    for fname in glob.glob(fname):
+        ipinfo =[]
+        for line in open(fname):
+            pat = "(ip address) ((?:[0-9]{1,3}[\.]){1,3}[0-9]{1,3}) ((?:[0-9]{1,3}[\.]){3}[0-9]{1,3})"
+            m = re.match(pat, line.strip().lower())
+            if m:
+                ipaddr = m.group(2) + "/" + m.group(3)
+                ipinfo.append(ipaddr)
+            if re.match("(hostname)", line.strip().lower()):
+                hname = line.strip().lower().split(" ")[1]
+        result[hname] = list(set(ipinfo))
+    return result
 
-    for f in configs:
-        host = ''
-        ip = []
-        with open(f) as file:
-
-            for s in file:
-
-                if re.match("^(hostname)", s):
-                    s = s.replace("hostname ", "")
-                    host = s.rstrip()
-
-                elif re.match("^( ip address) ([0-9]{1,3}\.?){4} ([0-9]{1,3}\.?){4}$", s):
-                    s = s.replace("ip address ", "")
-                    t = re.split(" ", s)
-                    ip.append(t[1])
-
-                hosts[host] = ip
-    return hosts
-
-h = Classifier(glob.glob("/config_files/*.txt"))
-
-print(h)
+res = getdatafromconfigs("config_files\*.txt")
 
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    s = """
-        /configs -  Host list 
-        /configs/hostname - Host IPs
-    """
-    return s
+    return "Go to /configs to see the hostnames; " \
+           "Go to /config/[name] to see [name] config. Thanks!"
+
+@app.route("/configs")
+def configs():
+    return jsonify(list(res.keys()))
 
 
-
+@app.route("/config/<name>")
+def config(name):
+    try:
+        return jsonify(res[name])
+    except:
+        return "No such page found"
 
 if __name__ == '__main__':
     app.run(debug=True)
