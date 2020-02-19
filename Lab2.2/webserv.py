cf2ecff9253b63258ce95f31c8e9b5b675748b58
aarask <andrey.rask@gmail.com>
2020-02-19 11:28:42 +0300

    Initial Test

--- Lab2.2/webserv.py
+++ Lab2.2/webserv.py
@@ -2,12 +2,13 @@ import re
 import glob
 from flask import Flask, jsonify
 
+filename = "config_files\*.txt"
 
-def getdatafromconfigs(fname):
+def getdatafromconfigs(filename):
     result = {}
-    for fname in glob.glob(fname):
-        ipinfo =[]
-        for line in open(fname):
+    for filename in glob.glob(filename):
+        ipinfo = []
+        for line in open(filename):
             pat = "(ip address) ((?:[0-9]{1,3}[\.]){1,3}[0-9]{1,3}) ((?:[0-9]{1,3}[\.]){3}[0-9]{1,3})"
             m = re.match(pat, line.strip().lower())
             if m:
@@ -18,7 +19,7 @@ def getdatafromconfigs(fname):
         result[hname] = list(set(ipinfo))
     return result
 
-res = getdatafromconfigs("config_files\*.txt")
+res = getdatafromconfigs(filename)
 
 
 app = Flask(__name__)
@@ -26,13 +27,12 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Go to /configs to see the hostnames; " \
-           "Go to /config/[name] to see [name] config. Thanks!"
+    return 'Go to <a href="/configs">/configs</a> to see the hostnames; ' \
+           'Go to /config/[name] to see [name] config. Thanks!'
 
 @app.route("/configs")
 def configs():
     return jsonify(list(res.keys()))
-
 
 @app.route("/config/<name>")
 def config(name):
@@ -40,6 +40,8 @@ def config(name):
         return jsonify(res[name])
     except:
         return "No such page found"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
